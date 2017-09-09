a270ca189c9c7a74c7b0c2fa32ea48c3accc321d
Prajit Patil <prajitpatil17@gamil.com>
2017-09-10 00:18:05 +0530

    Adding 'view profile' and 'edit profile' feature

--- expenseManager/splitx/views.py
+++ expenseManager/splitx/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
 from splitx.forms import RegistrationForm
 
 def home(request):
@@ -15,4 +16,16 @@ def register(request):
     else:
         form = RegistrationForm()
         return render(request, 'splitx/register_form.html', {'form':form})
-    
+
+def view_profile(request):
+    return render(request, 'splitx/profile.html', {'user': request.user})
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+        return redirect('/splitx/profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+        return render(request, 'splitx/edit_profile.html', {'form': form})
