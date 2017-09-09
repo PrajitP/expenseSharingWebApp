18a1d175f9dc4f2dde0ee0d2d85925d7498bab74
Prajit Patil <prajitpatil17@gamil.com>
2017-09-09 18:53:43 +0530

    Adding base template, basic home page and loging page

--- expenseManager/splitx/urls.py
+++ expenseManager/splitx/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
+    url(r'^login/$', login, {"template_name": 'splitx/login.html' }),
 ]
